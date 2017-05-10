75c75e391f7112e77644bc4342a2ae3024a12ca9
Jeanette Phung <jeanettephung@Jeanettes-MacBook-Pro.local>
2017-05-10 14:38:59 -0700

    fix compile error

--- Assets/Scripts/KitchenLoader.cs
+++ Assets/Scripts/KitchenLoader.cs
@@ -7,17 +7,8 @@ public class KitchenLoader : MonoBehaviour {
 	public string levelToLoad;
 
 	void OnMouseDown() {
-
-<<<<<<< HEAD
-		if (levelToLoad.Equals("home")) {
-			SceneManager.LoadScene ("home");
-
-			dialogue_home_intro.box.setDialogue (6);
-			dialogue_home_intro.box.setIntro (false);
-=======
 		if (levelToLoad == "back") {
 			SceneManager.LoadScene ("Kitchen");
->>>>>>> c6a3848f1488981cb515621dd80a060e88609643
 		} else {
 			SceneManager.LoadScene (levelToLoad);
 		}
