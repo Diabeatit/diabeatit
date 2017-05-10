51b64b1ac7a4764d18d9c002129fa7fad52fcc3f
Chandler Blaid Burgess <Eccmt70.cburgess@gmail.com>
2017-05-09 17:15:32 -0700

    Adding refrigerator scene (#1)

---
+++ Assets/Scripts/KitchenLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KitchenLoader : MonoBehaviour {
+	public string levelToLoad;
+
+	void OnMouseDown() {
+
+		if (levelToLoad == "back") {
+			SceneManager.LoadScene ("Kitchen");
+		} else {
+			SceneManager.LoadScene (levelToLoad);
+		}
+	}
+}
+
