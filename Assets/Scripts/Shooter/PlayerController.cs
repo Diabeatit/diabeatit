1c63970430026d3b19db44731799adc52b8ff189
C U D D Y <tcuddy@ucsd.edu>
2017-05-14 16:35:53 -0700

    we can Pick up foodObject

--- Assets/Scripts/Shooter/PlayerController.cs
+++ Assets/Scripts/Shooter/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public bool characterInQuicksand;
 	public GameObject laser; // this is the players laser prefab
 	public GameObject laserPosition;
 	public float speed;
@@ -25,7 +26,7 @@ public class PlayerController : MonoBehaviour {
 
 			// set the lasers initial position
 			laser1.transform.position = laserPosition.transform.position;
-			
+
 		}
 	}
 	void FixedUpdate () {
@@ -39,4 +40,21 @@ public class PlayerController : MonoBehaviour {
         rb2d.AddForce(movement*speed);
 
 	}
+
+    // set the boolean variable to true whenever we touch another object
+    // this method is called whenever our player touches another
+    // 2D trigger collider and it passes in a reference to the 2d object
+    // that it collided with
+    // So if the game object collided with has a tag of "Food" then we will
+    // set the GO active field to false
+    void OnTriggerEnter2D(Collider2D other) {
+       //characterInQuicksand = true;
+       if (other.gameObject.CompareTag("Food")){
+            other.gameObject.SetActive(false);
+            Destroy(other.gameObject);
+        }
+    }
+
+
+
 }
