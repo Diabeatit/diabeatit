93ad49183d019d1f8e45b5f58ef845bf1824318f
Gokul Suresh <gokul.suresh24@gmail.com>
2017-05-20 16:44:28 -0700

    changed layout of falling words game

---
+++ Assets/Scripts/FallingWords/QuestionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionManager : MonoBehaviour {
+
+	public GameObject textBox;
+	public TextAsset textFile;
+	public Text theText;
+	public string[] questions;
+
+	public int currentQuestion;
+	public int endQuestion;
+	 
+	// Use this for initialization
+	void Start () {
+		if (textFile != null) {
+			questions = (textFile.text.Split ('\n'));
+		}
+
+		if (endQuestion == 0) {
+			endQuestion = questions.Length - 1;
+		}
+	}	
+
+	void Update () {
+		theText.text = questions [currentQuestion];
+	}
+}
