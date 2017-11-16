49eaed85cefd336b9ca8d4b0aba47367edb3b6b3
Tyler Bakke <tylerbakke@gmail.com>
2017-11-15 17:34:57 -0800

    Updated Shooter scene and scripts

---
+++ Assets/Scripts/Shooter/gameDisplay.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class gameDisplay : MonoBehaviour {
+
+	private float time = 3f;
+
+	private static int count;
+
+	public Text countText;
+	public Text winText;
+
+	public Scene levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetCountText(){
+
+		countText.text = "count: " + getCount();
+
+		if (count >= 10) {
+			winText.text = "YOU WIN!!!";
+			if(time > 0){
+				time-=Time.deltaTime;
+			}else{
+				SceneManager.LoadScene ("home");
+			}
+		}
+	}
+
+	public void updateDisplay(gameObject.tag item){
+	
+	}
+
+	public static void incCount(){
+		count ++;
+	}
+	public static void decCount(){
+		count--;
+	}
+
+	public static string getCount(){
+		return count.ToString();
+	}
+}
