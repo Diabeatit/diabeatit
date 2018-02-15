e90016449a47f1b67daa4175db549fdb929c2e95
Daniel Kirby <dkirby@outlook.com>
2018-02-15 14:54:50 -0800

    Added simple transition image ph

--- Assets/Scripts/Shooter/popUp.cs
+++ Assets/Scripts/Shooter/popUp.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class popUp : MonoBehaviour {
-
+	//link this script to an image on the canvas
 	// Use this for initialization
 	void Start () {
+		//destroy transition image after 10 seconds have elapsed
 		Destroy(gameObject, 10f);
 	}
 }
