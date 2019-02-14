519edbd742564c8bc6028fde38289b311902255a
Yaman Jandali <yamanjandali@Yamans-MacBook-Pro.local>
2019-02-14 13:34:52 -0800

    Added pointer to fridge

---
+++ Assets/Scripts/wigglePointer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wigglePointer : MonoBehaviour
+{
+    private bool forward;
+    private float initPosX, initPosY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //InvokeRepeating("move", 1f, 0.00001f);
+        initPosX = transform.position.x;
+        initPosY = transform.position.y;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (forward)
+        {
+            transform.position = new Vector3((float)(transform.position.x + 0.005), (float)(transform.position.y + 0.005), transform.position.z);
+
+            if (transform.position.x > initPosX + .07)
+            {
+                forward = false;
+            }
+
+  
+          }
+        else
+        {
+            transform.position = new Vector3((float)(transform.position.x - 0.005), (float)(transform.position.y - 0.005), transform.position.z);
+
+            if (transform.position.x < initPosX - .07)
+            {
+                forward = true;
+            }
+        }
+
+
+
+
+    }
+}
