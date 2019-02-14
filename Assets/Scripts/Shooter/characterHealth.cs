a25bcd026db75ab8a0b4e794111e35b973ad0dd2
Yaman Jandali <yamanjandali@Yamans-MacBook-Pro.local>
2019-02-14 13:09:37 -0800

    Added red flashing when in red zone

--- Assets/Scripts/Shooter/characterHealth.cs
+++ Assets/Scripts/Shooter/characterHealth.cs
@@ -9,12 +9,21 @@ public class characterHealth : MonoBehaviour {
 	public static float maxHealth {get; set;}
 	public static Slider healthbar; 
 	public static Sprite arrow;
+    public Image redImage;
+    public float flashSpeed = 0.5f;
+    public Color flashColor = new Color(255f, 0f, 0f, 255f);
+    public Color defColor = new Color(0f, 0f, 0f, 0f);
+    public static bool unhealthy = false;
+    public static bool routine = true;
+    public static Text warning;
+    public int threshhold = 20;
 
 
-	void Start() {
+    void Start() {
 		healthbar = GameObject.Find("HealthBar").GetComponent<Slider>();
 		arrow = GameObject.Find ("arrow").GetComponent<Sprite> ();
-		maxHealth = 100f;
+        warning = GameObject.Find("Text").GetComponent<Text>();
+        maxHealth = 100f;
 		currentHealth = 50f;
 		healthbar.value = CalculateHealth();
 	}
@@ -33,9 +42,47 @@ public class characterHealth : MonoBehaviour {
 			currentHealth = 100f;
             Die();
 		}
-	}
+        else if (currentHealth <= (50-threshhold) || currentHealth >= (50+threshhold))
+        {
+            unhealthy = true;
 
-	public static void TakeDamage(float amount) {
+            if (routine == true)
+            {
+                StopCoroutine(flashRed());
+                unhealthy = true;
+                routine = false;
+                StartCoroutine(flashRed());
+            }
+
+        }
+
+        else
+        {
+            unhealthy = false;
+        }
+    }
+
+    IEnumerator flashRed()
+    {
+        print(unhealthy);
+        if (unhealthy == true)
+        {
+
+            print("unhealthy");
+            warning.text = "WARNING";
+            redImage.color = flashColor;
+            yield return new WaitForSeconds(flashSpeed);
+            warning.text = "";
+            redImage.color = defColor;
+            yield return new WaitForSeconds(flashSpeed);
+            routine = true;
+
+        }
+
+    }
+
+
+    public static void TakeDamage(float amount) {
 		currentHealth += amount;
 		healthbar.value = CalculateHealth ();
         
@@ -45,11 +92,12 @@ public class characterHealth : MonoBehaviour {
         if (currentHealth <= 0f) {
 			currentHealth = 0f;
 			Die();
-		}else if(currentHealth >= 100f){
+		}
+        else if(currentHealth >= 100f){
             currentHealth = 100f;
             Die();
         }
-	}
+    }
 
 	static float CalculateHealth() {
 		if ((currentHealth / maxHealth) > 1f) {
